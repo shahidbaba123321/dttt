86b5783b3fbe9a786960c9a542debdd86b676587
shahidbaba123321 <shahid.craftorbis@gmail.com>
2025-02-22 12:18:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -14,19 +14,7 @@ const nodemailer = require('nodemailer');
 const app = express();
 
 // CORS configuration middleware
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-    } else {
-        next();
-    }
-});
-
-// Basic middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(compression());
